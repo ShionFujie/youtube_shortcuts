d627d7be893c5d4aa8d2e4f79c823e6a856283f3
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-03 21:00:52 +0900

    add jquery

--- js/content.js
+++ js/content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(onMessage)
 
-function onMessage(request, _sender, sendResponse) {
-    sendResponse(`responded '${request}'`)
+function onMessage(request, _sender) {
+    $("[aria-owns='ytp-id-20']").click()
 }
