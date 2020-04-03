3d6cb49a7d2296a72e4cae63532ff8fe086b73fa
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-04 07:47:11 +0900

    doubled the playback speed

--- js/content.js
+++ js/content.js
@@ -2,4 +2,7 @@ chrome.runtime.onMessage.addListener(onMessage)
 
 function onMessage(request, _sender) {
     $("[aria-owns='ytp-id-20']").click()
+    var settingsMenu = $("#ytp-id-20")
+    settingsMenu.find(".ytp-menuitem:nth-child(2)").click()
+    settingsMenu.find(".ytp-menuitem:nth-child(8)").click()
 }
