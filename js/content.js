d446b3e218631c77897708ba9478f429a16f84b8
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-04 07:49:24 +0900

    add guard

--- js/content.js
+++ js/content.js
@@ -1,8 +1,10 @@
 chrome.runtime.onMessage.addListener(onMessage)
 
-function onMessage(request, _sender) {
-    $("[aria-owns='ytp-id-20']").click()
-    var settingsMenu = $("#ytp-id-20")
-    settingsMenu.find(".ytp-menuitem:nth-child(2)").click()
-    settingsMenu.find(".ytp-menuitem:nth-child(8)").click()
+function onMessage(command, _sender) {
+    if (command == 'playback_speed_2') {
+        $("[aria-owns='ytp-id-20']").click()
+        var settingsMenu = $("#ytp-id-20")
+        settingsMenu.find(".ytp-menuitem:nth-child(2)").click()
+        settingsMenu.find(".ytp-menuitem:nth-child(8)").click()
+    }   
 }
