cba3ce1f707dd05c119b4e0632e260043515163a
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-04 09:33:50 +0900

    neutralize the playback speed

--- js/content.js
+++ js/content.js
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener(onMessage)
 
 function onMessage(command, _sender) {
-    if (command == 'playback_speed_2') {
+    if (command == 'playback_speed_2' || command == 'playback_speed_normal') {
         var settingsMenu = $("#ytp-id-20")
+        var menuItemIdx = command == 'playback_speed_normal' ? 4 : 8
         // Clicks the settings button on the bottom controls of the YouTube video
         $("[aria-owns='ytp-id-20']").click()
-        // A popup with menu items shown, then clicks the 'Playback speed' item
+        // A popup with menu items shown, then clicks the 'Playback speed' item\
         settingsMenu.find(".ytp-menuitem:has(.ytp-menuitem-label:contains('Playback speed'))")
             .click()
-        // The popup re-rendered with new menu items, then clicks the '2' item
-        settingsMenu.find(".ytp-menuitem:nth-child(8)").click()
+        // The popup re-rendered with new menu items, then clicks the '2' or 'Normal' item
+        settingsMenu.find(`.ytp-menuitem:nth-child(${menuItemIdx})`).click()
         // Clicks outside, resulting in closing the popup
         $("#container").click()
     }   
