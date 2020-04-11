88e7c9d0fcbf69c0cf32fb7f97ff7779ccf508d5
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-11 16:43:22 +0900

    separate utility

--- js/background.js
+++ js/background.js
@@ -29,26 +29,8 @@ function executeDoublePlaybackSpeed() {
   executeAfterLoadingScript("doublePlaybackSpeed()");
 }
 
-function navigateToPath(pathname) {
-  chrome.tabs.executeScript({
-    code: `document.location.pathname = '${pathname}'`
-  });
-}
-
-function queryActiveTab(onActiveTab) {
-  chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-    onActiveTab(activeTab);
-  });
-}
-
 function executeAfterLoadingScript(code) {
   executeScriptWithJQuery("js/setPlaybackSpeed.js", () => {
     chrome.tabs.executeScript({ code });
-  });
-}
-
-function executeScriptWithJQuery(file, callback) {
-  chrome.tabs.executeScript({ file: "lib/jquery-3.4.1.min.js" }, () => {
-    chrome.tabs.executeScript({ file }, callback);
   });
 }
