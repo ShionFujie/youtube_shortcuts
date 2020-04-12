de0423ba94cba4e47688a64274fc4cd9e4683e03
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-12 20:29:41 +0900

    view watch later playlist

--- js/content.js
+++ js/content.js
@@ -2,9 +2,11 @@ const KEY_CODE_PLAYBACK_SPEED_NORMAL = "KeyN";
 const KEY_CODE_PLAYBACK_SPEED_2 = "KeyD";
 const KEY_CODE_GO_HOME = "KeyH";
 const KEY_CODE_HISTORY = "KeyY";
+const KEY_CODE_WATCH_LATER = "KeyW";
 
-const { pathname } = location;
-document.onkeydown = ({ code }) => {
+const url = new URL(location.href)
+const { pathname} = url;
+document.onkeydown = ({ code, search }) => {
   if (
     [KEY_CODE_PLAYBACK_SPEED_NORMAL, KEY_CODE_PLAYBACK_SPEED_2].includes(
       code
@@ -16,4 +18,12 @@ document.onkeydown = ({ code }) => {
   } else if (code == KEY_CODE_GO_HOME && pathname != "/") navigateToPath("/");
   else if (code == KEY_CODE_HISTORY && pathname != "/feed/history")
     navigateToPath("/feed/history");
+  else if (
+    code == KEY_CODE_WATCH_LATER &&
+    !(pathname == "/playlist" && search == "?list=WL")
+  ) {
+    url.pathname = "/playlist"
+    url.search = "?list=WL"
+    location.href = url.toString()
+  }
 };
