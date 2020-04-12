637ff944213a0003f6999c7961cf6e998791de62
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-12 20:43:23 +0900

    expand navigateToPath() to updateLocation()

--- js/content.js
+++ js/content.js
@@ -4,8 +4,8 @@ const KEY_CODE_GO_HOME = "KeyH";
 const KEY_CODE_HISTORY = "KeyY";
 const KEY_CODE_WATCH_LATER = "KeyW";
 
-const url = new URL(location.href)
-const { pathname} = url;
+const url = new URL(location.href);
+const { pathname } = url;
 document.onkeydown = ({ code, search }) => {
   if (
     [KEY_CODE_PLAYBACK_SPEED_NORMAL, KEY_CODE_PLAYBACK_SPEED_2].includes(
@@ -15,15 +15,13 @@ document.onkeydown = ({ code, search }) => {
   ) {
     if (code == KEY_CODE_PLAYBACK_SPEED_NORMAL) neutralizePlaybackSpeed();
     else if (code == KEY_CODE_PLAYBACK_SPEED_2) doublePlaybackSpeed();
-  } else if (code == KEY_CODE_GO_HOME && pathname != "/") navigateToPath("/");
+  } else if (code == KEY_CODE_GO_HOME && pathname != "/")
+    updateLocation(url, { pathname: "/" });
   else if (code == KEY_CODE_HISTORY && pathname != "/feed/history")
-    navigateToPath("/feed/history");
+    updateLocation(url, { pathname: "/feed/history" });
   else if (
     code == KEY_CODE_WATCH_LATER &&
     !(pathname == "/playlist" && search == "?list=WL")
-  ) {
-    url.pathname = "/playlist"
-    url.search = "?list=WL"
-    location.href = url.toString()
-  }
+  )
+    updateLocation(url, { pathname: "/playlist", search: "?list=WL" });
 };
