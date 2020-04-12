e20a0d8bf0295c65bbe1d935deec9e0ad80f1d47
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-12 19:09:56 +0900

    removed unused functions

--- js/util/utils.js
+++ js/util/utils.js
@@ -1,15 +1,3 @@
-function queryActiveTab(onActiveTab) {
-  chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-    onActiveTab(activeTab);
-  });
-}
-
-function executeScriptWithJQuery(file, callback) {
-  chrome.tabs.executeScript({ file: "lib/jquery-3.4.1.min.js" }, () => {
-    chrome.tabs.executeScript({ file }, callback);
-  });
-}
-
 function navigateToPath(pathname) {
   document.location.pathname = pathname
 }
