48a78185ea830a4a05951de8524e377ce27376c5
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-21 20:47:48 +0900

    implement extraction of image URL

--- js/img-url.js
+++ js/img-url.js
@@ -5,19 +5,42 @@ function setup() {
   injectCSS();
 }
 
+function teardown() {
+  removeListeners();
+  removeCSS();
+}
+
 function addListeners() {
-  // TODO
+  for (const el of document.querySelectorAll("ytd-thumbnail")) {
+    el.addEventListener("click", extractURL);
+  }
 }
 
 function removeListeners() {
-  // TODO
+  for (const el of document.querySelectorAll("ytd-thumbnail")) {
+    el.removeEventListener("click", extractURL);
+  }
+}
+
+function extractURL(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var el = e.target;
+  // Look up the container anchor
+  while (el.tagName != "A") {
+    el = el.parentElement;
+  }
+  const newClip = el.querySelector("yt-img-shadow #img").src;
+  navigator.clipboard.writeText(newClip);
+  teardown();
 }
 
 function injectCSS() {
   const style = document.createElement("style");
-  const css = " ytd-thumbnail #thumbnail:hover { box-sizing: border-box; border-style: solid; border-radius: 2px; border-color: #4285f4; }"
-  const styleText = document.createTextNode(css)
-  style.id = 'shionfujie.youtube:style'
+  const css =
+    " ytd-thumbnail #thumbnail:hover { box-sizing: border-box; border-style: solid; border-radius: 2px; border-color: #4285f4; }";
+  const styleText = document.createTextNode(css);
+  style.id = "shionfujie.youtube:style";
   style.appendChild(styleText);
   document.getElementsByTagName("head")[0].appendChild(style);
 }
@@ -25,9 +48,4 @@ function injectCSS() {
 function removeCSS() {
   var cssNode = document.getElementById("shionfujie.youtube:style");
   cssNode && cssNode.parentNode.removeChild(cssNode);
-}
-
-function teardown() {
-  removeListeners();
-  removeCSS();
 }
