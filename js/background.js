fb8b91d4dfaa7c322806585c4a75f43401bac866
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-21 18:15:10 +0900

    Expose Public API

---
+++ js/background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onMessageExternal.addListener((request, _, response) => {
+    if (request.type === "action spec") {
+      response({
+        name: actionSpec.name,
+        actions: Object.entries(actionSpec.actions).map(
+          ([name, { displayName }]) => {
+            return { name, displayName };
+          }
+        )
+      });
+    } else if (request.type === "execute action") {
+      const action = actionSpec.actions[request.action.name];
+      if (action !== undefined) action.f();
+    }
+  });
+  
+  const actionSpec = {
+    name: "YouTube",
+    actions: {
+      "img url": {
+        displayName: "YouTube: Copy URL of Thumbnail",
+        f: requestCopyThumbnailURL
+      }
+    }
+  };
+
+  function requestCopyThumbnailURL() {
+      
+  }
